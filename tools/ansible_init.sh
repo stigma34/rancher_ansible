fe578ab0189682c6bcf83baf6ebc7bf1221259a6
Shawn Andrews <shawn@notcreative.cloud>
2025-11-21 19:05:24 -0600

    add ansible init

---
+++ tools/ansible_init.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+echo "[+] Updating system packages..."
+dnf -y update
+
+echo "[+] Installing required system packages..."
+dnf -y install python3 python3-pip python3-devel gcc openssl-devel git
+
+echo "[+] Creating Ansible virtual environment..."
+python3 -m venv ~/ansible-venv
+
+echo "[+] Activating virtual environment..."
+source ~/ansible-venv/bin/activate
+
+echo "[+] Upgrading pip and installing Ansible..."
+pip install --upgrade pip wheel
+pip install ansible ansible-lint
+
+echo ""
+echo "[+] Ansible installation complete!"
+echo "[+] To activate your Ansible environment in the future, run:"
+echo "     source ~/ansible-venv/bin/activate"
+echo ""
+echo "[+] To deactivate:"
+echo "     deactivate"
+echo ""
+echo "[+] Version check:"
+ansible --version
+
